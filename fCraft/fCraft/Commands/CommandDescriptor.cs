ad55d46526ffa75f1baf334a2b2f8577c21d9c47
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2010-08-04 04:38:46 +0000

    Included missing files

---
+++ fCraft/fCraft/Commands/CommandDescriptor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft {
+
+    public delegate void CommandHandler( Player source, Command message );
+
+    public delegate string HelpHandler( Player source );
+
+    public sealed class CommandDescriptor {
+        public string name;                 // main name
+        public string[] aliases;            // list of aliases
+        public bool consoleSafe;            // if true, command can be called from console
+        public Permission[] permissions;    // list of required permissions
+        public string usage;                // short help
+        public string help;                 // full help
+        public CommandHandler handler;      // callback function to execute the command
+        public HelpHandler helpHandler;     // callback function to provide custom help (optional)
+
+        public void PrintUsage( Player player ) {
+            if( usage != null ) {
+                player.Message( "Usage: " + Color.Help + usage );
+            } else {
+                player.Message( "Usage: " + Color.Help + "/" + name );
+            }
+        }
+    }
+}
