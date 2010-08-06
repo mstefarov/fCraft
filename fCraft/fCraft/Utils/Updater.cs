8249be47911924ecb041bb2a8f1c1dee2ea8bf3b
Matvei Stefarov <me@matvei.org>
2010-08-06 00:41:45 +0000

    Final fix for the /worlds problem. Sorry about that.

--- fCraft/fCraft/Utils/Updater.cs
+++ fCraft/fCraft/Utils/Updater.cs
@@ -27,8 +27,8 @@ namespace fCraft {
 
 
     public static class Updater {
-        static int version = 430;
-        static int revision = 144;
+        static int version = 431;
+        static int revision = 146;
 
         public static UpdaterResult CheckForUpdates() {
             UpdaterResult result = new UpdaterResult( version );
