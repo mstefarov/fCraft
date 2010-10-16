450ad9b2dc5579dd504a74f70e4310721d422476
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2010-10-16 04:07:36 +0000

    Fix: Fixed an extremely embarrassing bug in /info that displayed incorrect "total time on server".

--- trunk/fCraft/Utils/Updater.cs
+++ trunk/fCraft/Utils/Updater.cs
@@ -28,7 +28,7 @@ namespace fCraft {
 
     public static class Updater {
         public static int Version = 490;
-        public static int Revision = 266;
+        public static int Revision = 267;
         public static bool IsUnstable = false;
 
         public static UpdaterResult CheckForUpdates() {
