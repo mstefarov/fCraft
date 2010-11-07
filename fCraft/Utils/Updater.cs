c4c302ba9a349d92c2df044e17952b45b039ba1d
Matvei Stefarov <me@matvei.org>
2010-11-07 01:30:40 +0000

    Fix: Fixed "Flatgrass" tab not working in AddWorldPopup  Fix: Fixed alphabetical order in the template list.

--- fCraft/Utils/Updater.cs
+++ fCraft/Utils/Updater.cs
@@ -28,7 +28,7 @@ namespace fCraft {
 
     public static class Updater {
         public static int Version = 499;
-        public static int Revision = 311;
+        public static int Revision = 312;
         public static bool IsUnstable = false;
 
         public static UpdaterResult CheckForUpdates() {
