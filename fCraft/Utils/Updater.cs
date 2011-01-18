96fd330b1fe75b33cac9751e80e2a30f348ffc06
Matvei Stefarov <me@matvei.org>
2011-01-18 20:08:16 +0000

    Fixed the fix for shutdown errors :|

--- fCraft/Utils/Updater.cs
+++ fCraft/Utils/Updater.cs
@@ -32,10 +32,10 @@ namespace fCraft {
     /// </summary>
     public static class Updater {
         public static int Version = 500;
-        public static int Revision = 385;
+        public static int Revision = 386;
         public static bool IsDev = true,
                            IsBroken = false;
-        public static string LatestNonBroken = "0.500_r385_dev";
+        public static string LatestNonBroken = "0.500_r386_dev";
 
         const string UpdateURL = "http://fcraft.fragmer.net/version.log";
 
