0bd23abd1f7eed1bd70ae4599d90e22219c4d794
Matvei Stefarov <me@matvei.org>
2011-02-14 04:56:48 +0000

    Further cleanup in Map.SaveBackup.

--- fCraftWinService/Properties/AssemblyInfo.cs
+++ fCraftWinService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "0.5.0.5" )]
-[assembly: AssemblyFileVersion( "0.5.0.5" )]
+[assembly: AssemblyVersion( "0.5.0.7" )]
+[assembly: AssemblyFileVersion( "0.5.0.7" )]
