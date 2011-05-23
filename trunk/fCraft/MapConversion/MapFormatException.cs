2da51da8de0581787b77c6b66c17a3b536da76a0
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-05-23 04:28:26 +0000

    Discarded /trunk/ modifications so far. I will continue work on branch-0.53x

--- trunk/fCraft/MapConversion/MapFormatException.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright 2009, 2010, 2011 Matvei Stefarov <me@matvei.org>
-using System;
-
-namespace fCraft.MapConversion {
-    public sealed class MapFormatException : Exception {
-        public MapFormatException() { }
-        public MapFormatException( string message ) : base( message ) { }
-    }
-}
