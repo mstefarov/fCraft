178b10c3ed3e58ddc4501766b8ccedd8866d0423
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-06-02 02:29:59 +0000

    Added missing PermissionLimitBox classes.

---
+++ branch-0.60x/ConfigTool/PermissionLimitBox.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using fCraft;
+
+namespace ConfigTool {
+    public partial class PermissionLimitBox : UserControl {
+
+        public Permission Permission { get; private set; }
+
+        public string FirstItem { get; private set; }
+
+        public Rank Rank { get; private set; }
+
+        public PermissionLimitBox( string labelText, Permission permission, string firstItem ) {
+            InitializeComponent();
+
+            label.Text = labelText;
+            label.Left = (comboBox.Left - comboBox.Margin.Left) - (label.Width + label.Margin.Right);
+
+            Permission = permission;
+            FirstItem = firstItem;
+            RebuildList();
+
+            comboBox.SelectedIndexChanged += OnPermissionLimitChanged;
+        }
+
+
+        void OnPermissionLimitChanged( object sender, EventArgs args ) {
+            if( Rank == null ) return;
+            Rank rankLimit = RankManager.FindRank( comboBox.SelectedIndex - 1 );
+            if( rankLimit == null ) {
+                Rank.ResetLimit( Permission );
+            } else {
+                Rank.SetLimit( Permission, rankLimit );
+            }
+        }
+
+
+        public void Reset() {
+            comboBox.SelectedIndex = 0;
+        }
+
+
+        public void RebuildList() {
+            comboBox.Items.Clear();
+            comboBox.Items.Add( FirstItem );
+            foreach( Rank rank in RankManager.Ranks ) {
+                comboBox.Items.Add( rank.ToComboBoxOption() );
+            }
+        }
+
+
+        public void SelectRank( Rank rank ) {
+            Rank = rank;
+            if( rank == null ) {
+                comboBox.SelectedIndex = -1;
+                Visible = false;
+            } else {
+                comboBox.SelectedIndex = rank.GetLimitIndex( Permission );
+                Visible = rank.Can( Permission );
+            }
+        }
+
+
+        public void PermissionToggled( bool isOn ) {
+            Visible = isOn;
+        }
+    }
+}
