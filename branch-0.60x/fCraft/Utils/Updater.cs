44cc0fb252b189bca2a562fb5b855bf37d01ad41
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-06-24 04:18:53 +0000

    Slight refactoring of PlayerPlacingBlock/PlayerPlacedBlock.

--- branch-0.60x/fCraft/Utils/Updater.cs
+++ branch-0.60x/fCraft/Utils/Updater.cs
@@ -17,7 +17,7 @@ namespace fCraft {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             600,
-            674,
+            679,
             new DateTime( 2011, 6, 19, 2, 30, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Dev | ReleaseFlags.Unstable
