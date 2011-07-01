06f05a7a15aeabbd9324554e4938f32ea06d7629
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-07-01 18:38:45 +0000

    Feature: Included a prototype of a visual AutoRank.xml editor. Fix: Included missing IClassy.cs file to the fCraft project.

---
+++ branch-0.60x/AutoRankEditor/Enums.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoRankEditor {
+    public enum ComparisonOperation {
+        eq,
+        neq,
+        gt,
+        gte,
+        lt,
+        lte
+    }
+
+
+    public enum ConditionField {
+        TimeSinceFirstLogin,
+        TimeSinceLastLogin,
+        LastSeen,
+        TotalTime,
+        BlocksBuilt,
+        BlocksDeleted,
+        BlocksChanged, // BlocksBuilt+BlocksDeleted
+        BlocksDrawn,
+        TimesVisited,
+        MessagesWritten,
+        TimesKicked,
+        TimeSinceRankChange,
+        TimeSinceLastKick
+    }
+
+
+    public enum ConditionScopeType {
+        Total,
+        SinceRankChange,
+        SinceKick
+    }
+
+
+    public enum ActionType {
+        Suggested,
+        Required,
+        Automatic
+    }
+
+
+    public enum RankChangeType {
+        Default,
+        Promoted,
+        Demoted,
+        AutoPromoted,
+        AutoDemoted
+    }
+}
