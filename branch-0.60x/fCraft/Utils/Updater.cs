530e47c118277ac84303ef113a0cf977dac47b63
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-07-26 01:32:45 +0000

    Fixed /wload and /gen not updating the scheduler tasks (which rendered worlds uneditable).

--- branch-0.60x/fCraft/Utils/Updater.cs
+++ branch-0.60x/fCraft/Utils/Updater.cs
@@ -17,7 +17,7 @@ namespace fCraft {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             600,
-            786,
+            791,
             new DateTime( 2011, 7, 25, 20, 40, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Dev
