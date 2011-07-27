70accaf19fdfb9262be40b68775a99ace76e4810
Matvei Stefarov <me@matvei.org>
2011-07-27 22:39:37 +0000

    More work on /ehx. It kinda works if both blocks are given (inner and outer). Not fully functional yet though.

--- fCraft/Drawing/DrawOps/EllipsoidHollowDrawOperation.cs
+++ fCraft/Drawing/DrawOps/EllipsoidHollowDrawOperation.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 
 namespace fCraft.Drawing {
     public class EllipsoidHollowDrawOperation : DrawOperation {
-        Vector3F Radius, Center;
-        bool fillInner;
 
         public override string Name {
             get { return "EllipsoidHX"; }
@@ -27,13 +25,13 @@ namespace fCraft.Drawing {
             double ry = Bounds.Length / 2d;
             double rz = Bounds.Height / 2d;
 
-            Radius.X = (float)(1 / (rx * rx));
-            Radius.Y = (float)(1 / (ry * ry));
-            Radius.Z = (float)(1 / (rz * rz));
+            radius.X = (float)(1 / (rx * rx));
+            radius.Y = (float)(1 / (ry * ry));
+            radius.Z = (float)(1 / (rz * rz));
 
-            Center.X = (Bounds.XMin + Bounds.XMax) / 2f;
-            Center.Y = (Bounds.YMin + Bounds.YMax) / 2f;
-            Center.Z = (Bounds.ZMin + Bounds.ZMax) / 2f;
+            center.X = (Bounds.XMin + Bounds.XMax) / 2f;
+            center.Y = (Bounds.YMin + Bounds.YMax) / 2f;
+            center.Z = (Bounds.ZMin + Bounds.ZMax) / 2f;
 
             fillInner = Brush.HasAlternateBlock &&
                         Bounds.Width > 2 &&
@@ -48,11 +46,109 @@ namespace fCraft.Drawing {
                                                                 (rx - .5) * (ry - .5) * (rz - .5)) * 0.85);
             }
 
-            coordEnumerator = BlockEnumerator().GetEnumerator();
+            //coordEnumerator = BlockEnumerator().GetEnumerator();
             return true;
         }
 
 
+        Vector3F radius, center;
+        bool fillInner;
+
+        enum State {
+            BeforeBlock = 0,
+            OuterBlock1 = 1,
+            OuterBlock2 = 2,
+            AfterOuterBlock = 3,
+            InnerBlock = 4
+        }
+
+        State state;
+        double dx, dy, dz;
+        public override int DrawBatch( int maxBlocksToDraw ) {
+            int blocksDone = 0;
+            for( ; Coords.X <= Bounds.XMax; Coords.X++ ) {
+                for( ; Coords.Y <= Bounds.YMax; Coords.Y++ ) {
+                    for( ; Coords.Z <= Bounds.ZMax; Coords.Z++ ) {
+                        switch( state ) {
+                            case State.BeforeBlock:
+                                state = State.BeforeBlock;
+                                dx = (Coords.X - center.X);
+                                dy = (Coords.Y - center.Y);
+                                dz = (Coords.Z - center.Z);
+                                if( (dx * dx) * radius.X + (dy * dy) * radius.Y + (dz * dz) * radius.Z > 1 ) continue;
+                                goto case State.OuterBlock1;
+
+                            case State.OuterBlock1:
+                                state = State.OuterBlock1;
+                                if( DrawOneBlock() ) {
+                                    blocksDone++;
+                                }
+                                goto case State.OuterBlock2;
+
+                            case State.OuterBlock2: {
+                                    state = State.OuterBlock2;
+                                    if( blocksDone >= maxBlocksToDraw ) return blocksDone;
+                                    int z = Coords.Z;
+                                    Coords.Z = (int)(center.Z - dz);
+                                    if( DrawOneBlock() ) {
+                                        blocksDone++;
+                                    }
+                                    Coords.Z = z;
+                                    goto case State.AfterOuterBlock;
+                                }
+
+                            case State.AfterOuterBlock:
+                                state = State.AfterOuterBlock;
+                                if( blocksDone >= maxBlocksToDraw ) return blocksDone;
+                                dz = (++Coords.Z - center.Z);
+                                if( Coords.Z <= (int)center.Z &&
+                                 ((dx + 1) * (dx + 1) * radius.X + (dy * dy) * radius.Y + (dz * dz) * radius.Z > 1 ||
+                                  (dx - 1) * (dx - 1) * radius.X + (dy * dy) * radius.Y + (dz * dz) * radius.Z > 1 ||
+                                  (dx * dx) * radius.X + (dy + 1) * (dy + 1) * radius.Y + (dz * dz) * radius.Z > 1 ||
+                                  (dx * dx) * radius.X + (dy - 1) * (dy - 1) * radius.Y + (dz * dz) * radius.Z > 1 ||
+                                  (dx * dx) * radius.X + (dy * dy) * radius.Y + (dz + 1) * (dz + 1) * radius.Z > 1 ||
+                                  (dx * dx) * radius.X + (dy * dy) * radius.Y + (dz - 1) * (dz - 1) * radius.Z > 1) ) {
+                                    goto case State.OuterBlock1;
+                                }
+
+                                if( !fillInner ) {
+                                    state = State.BeforeBlock;
+                                    continue;
+                                }
+
+                                UseAlternateBlock = true;
+                                goto case State.InnerBlock;
+
+                            case State.InnerBlock:
+                                state = State.InnerBlock;
+                                if( Coords.Z > (int)(center.Z - dz) ) {
+                                    UseAlternateBlock = false;
+                                    state = State.BeforeBlock;
+                                    continue;
+                                }
+                                if( DrawOneBlock() ) {
+                                    blocksDone++;
+                                    Coords.Z++;
+                                    if( blocksDone >= maxBlocksToDraw ) {
+                                        return blocksDone;
+                                    }
+                                } else {
+                                    Coords.Z++;
+                                }
+                                goto case State.InnerBlock;
+
+                        }
+                    }
+                    Coords.Z = Bounds.ZMin;
+                }
+                Coords.Y = Bounds.YMin;
+            }
+            IsDone = true;
+            return blocksDone;
+        }
+
+
+        /*
         IEnumerator<Vector3I> coordEnumerator;
         public override int DrawBatch( int maxBlocksToDraw ) {
             int blocksDone = 0;
@@ -66,11 +162,9 @@ namespace fCraft.Drawing {
             IsDone = true;
             return blocksDone;
         }
-
+        */
 
         IEnumerable<Vector3I> BlockEnumerator() {
-            Vector3F center = Center,
-                     radius = Radius;
             for( int x = Bounds.XMin; x <= Bounds.XMax; x++ ) {
                 for( int y = Bounds.YMin; y <= Bounds.YMax; y++ ) {
                     for( int z = Bounds.ZMin; z <= Bounds.ZMax; z++ ) {
@@ -101,6 +195,7 @@ namespace fCraft.Drawing {
                             for( ; z <= (int)(center.Z - dz); z++ ) {
                                 yield return new Vector3I( x, y, z );
                             }
+                            UseAlternateBlock = false;
                         }
                     }
                 }
