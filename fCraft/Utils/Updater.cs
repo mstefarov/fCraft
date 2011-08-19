727619acf1627dce7b87dcb8e71714c7e3c2f05e
Matvei Stefarov <me@matvei.org>
2011-08-19 04:20:32 +0000

    Fix: When sending rank messages (@@rank) to a rank other than your own, the message is now displayed to you. Fix: Fixed rank chat being logged twice.

--- fCraft/Utils/Updater.cs
+++ fCraft/Utils/Updater.cs
@@ -17,7 +17,7 @@ namespace fCraft {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             600,
-            897,
+            902,
             new DateTime( 2011, 8, 16, 3, 20, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Dev
