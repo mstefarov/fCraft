11864bd665000977b718fbb5bbfdcd74ff3715e0
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-08-29 19:28:22 +0000

    Further improvements and fixes to per-world backup settings.

--- branch-0.60x/fCraft/Utils/Updater.cs
+++ branch-0.60x/fCraft/Utils/Updater.cs
@@ -17,8 +17,8 @@ namespace fCraft {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             600,
-            913,
-            new DateTime( 2011, 8, 23, 3, 20, 0, DateTimeKind.Utc ),
+            916,
+            new DateTime( 2011, 8, 29, 19, 30, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Dev
 #if DEBUG
