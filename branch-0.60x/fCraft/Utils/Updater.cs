731eb5d9786a7998fb1321b948cd65b765689692
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-08-31 03:53:26 +0000

    Updated the version information.

--- branch-0.60x/fCraft/Utils/Updater.cs
+++ branch-0.60x/fCraft/Utils/Updater.cs
@@ -17,7 +17,7 @@ namespace fCraft {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             600,
-            916,
+            920,
             new DateTime( 2011, 8, 29, 19, 30, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Dev
