e6fe3fa8351ce31da5287a9f4b96f60134f60e7b
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-09-14 20:29:16 +0000

    0.537 release.  Fixed another heartbeat-related issue caused by minecraft.net updates.

--- branch-0.53x/fCraft/Utils/Updater.cs
+++ branch-0.53x/fCraft/Utils/Updater.cs
@@ -18,9 +18,9 @@ namespace fCraft {
     public static class Updater {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
-            536,
-            989,
-            new DateTime( 2011, 9, 14, 19, 30, 0, DateTimeKind.Utc ),
+            537,
+            991,
+            new DateTime( 2011, 9, 14, 20, 30, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Bugfix
 #if DEBUG
@@ -28,7 +28,7 @@ namespace fCraft {
 #endif
         );
 
-        public const string LatestStable = "0.536_r989";
+        public const string LatestStable = "0.537_r991";
 
         public static string UpdateUrl { get; set; }
 
--- branch-0.53x/fCraftUI/Properties/AssemblyInfo.cs
+++ branch-0.53x/fCraftUI/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.5.3.6" )]
-[assembly: AssemblyFileVersion( "0.5.3.6" )]
+[assembly: AssemblyVersion( "0.5.3.7" )]
+[assembly: AssemblyFileVersion( "0.5.3.7" )]
 
 [assembly: CLSCompliant( false )]
 [assembly: NeutralResourcesLanguage( "en-US" )]
