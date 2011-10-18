9efff252241f6c0b045ab5b94e2e7785b2c47cff
Matvei Stefarov <me@matvei.org>
2011-10-18 00:05:58 +0000

    Fixed a typo in ConfigGUI keyword picker. Fixed a bug in BoundingBox.Intersects(BoundingBox) Fixed /paste and /pastenot not properly clipping against world boundaries.

--- ConfigGUI/KeywordPicker.cs
+++ ConfigGUI/KeywordPicker.cs
@@ -6,13 +6,13 @@ namespace fCraft.ConfigGUI {
 
         readonly ToolTip tips;
         static readonly KeywordInfo[] Keywords = new[]{
-            new KeywordInfo( "{SERVER_NAME}", "Server name", "Name of your server, as specified in config." ),
+            new KeywordInfo("{SERVER_NAME}", "Server name", "Name of your server, as specified in config." ),
             new KeywordInfo("{RANK}", "Player's rank", "Player's rank, including prefix and colors (if applicable)." ),
             new KeywordInfo("{PLAYER_NAME}", "Player's name", "Name of the player, including prefix and colors (if applicable)." ),
             new KeywordInfo("{TIME}", "Time", "Current time (server clock)."),
             new KeywordInfo("{WORLD}", "Main world name","Name of the main/starting world, including prefix and colors (if applicable)." ),
             new KeywordInfo("{PLAYERS}", "Number of players online", "Note that hidden players will not be included in this number." ),
-            new KeywordInfo("{WORLDS}", "Number of worlds", "Number of worlds accessible by the player. Does not could hidden worlds." ),
+            new KeywordInfo("{WORLDS}", "Number of worlds", "Number of worlds accessible by the player. Does not count hidden worlds." ),
             new KeywordInfo("{MOTD}", "MOTD", "Message-of-the-day (server subtitle), as specified in config." ),
             new KeywordInfo("{VERSION}", "fCraft version", "Version of fCraft that this server is running." )
         };
