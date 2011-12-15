4f12397900d334c553ee1f5619d484dcfe89530f
Matvei Stefarov <me@matvei.org>
2011-12-15 21:12:23 +0000

    Misc code cleanup, fixed typos and formatting.

--- fCraft/World/Block.cs
+++ fCraft/World/Block.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright 2009, 2010, 2011 Matvei Stefarov <me@matvei.org>
 
-
 namespace fCraft {
     /// <summary> Enumeration of all standard Minecraft Classic block types. </summary>
     public enum Block : byte {
