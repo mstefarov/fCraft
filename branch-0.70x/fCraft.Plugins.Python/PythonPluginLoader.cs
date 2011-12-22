787a58e23d01e3c7dc3f8712f51eee83013d0b9a
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-12-22 19:11:04 +0000

    Added fCraft.Plugins.Python project.

---
+++ branch-0.70x/fCraft.Plugins.Python/PythonPluginLoader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using fCraft;
+using fCraft.Events;
+
+using IronPython.Hosting;
+using IronPython.Runtime.Types;
+
+using Microsoft.Scripting.Hosting;
+using IronPython.Runtime.Operations;
+
+
+namespace fCraft.Plugins.Python {
+    /// <summary> Loads IronPython plugin files. </summary>
+    public class PythonPluginLoader : IPluginLoader {
+        private ScriptRuntime _ironPythonRuntime;
+        private ScriptEngine _ironPythonEngine;
+
+        public PythonPluginLoader() {
+            _ironPythonEngine = IronPython.Hosting.Python.CreateEngine();
+            _ironPythonRuntime = _ironPythonEngine.Runtime;
+            _ironPythonRuntime.LoadAssembly(typeof(Server).Assembly);
+        }
+
+        public string[] PluginExtensions {
+            get { return new [] { ".py" }; }
+        }
+
+        public PluginLoadResult LoadPlugins(string filePath) {
+            try {
+                ScriptSource script = _ironPythonEngine.CreateScriptSourceFromFile(filePath);
+                CompiledCode code = script.Compile();
+                ScriptScope scope = _ironPythonEngine.CreateScope();
+                code.Execute(scope);
+
+                List<IPlugin> plugins = new List<IPlugin>();
+                foreach (KeyValuePair<string, dynamic> kvp in scope.GetItems().Where(kvp => kvp.Value is PythonType)) {
+                    dynamic value = kvp.Value;
+                    if (PythonOps.IsSubClass(value, DynamicHelpers.GetPythonTypeFromType(typeof(IPlugin)))) {
+                        IPlugin plugin = value();
+                        plugins.Add(plugin);
+                    }
+                }
+                return new PluginLoadResult() {
+                    LoadedPlugins = plugins,
+                    LoadSuccessful = true, 
+                };
+
+            } catch (Exception e) {
+                return new PluginLoadResult() {
+                    Exception = e,
+                    LoadSuccessful = false
+                };
+            }
+        }
+
+        private void LoadAssemblies() {
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+                _ironPythonRuntime.LoadAssembly(assembly);
+            }
+        }
+    }
+}
