e1b46b4fad566ab6c8aecec14790a85b244e5a00
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2011-12-25 17:35:28 +0000

    Fixed FlatfilePlayerDBProvider incorrectly implementing Import functionality. Improved IPlayerDBProviders' documentation.

--- branch-0.70x/fCraft.Core/PlayerDB/FlatfilePlayerDBProvider.cs
+++ branch-0.70x/fCraft.Core/PlayerDB/FlatfilePlayerDBProvider.cs
@@ -47,6 +47,8 @@ namespace fCraft {
         readonly Trie<PlayerInfo> trie = new Trie<PlayerInfo>();
 
 
+        /// <summary> Adds a new PlayerInfo entry for an actual, logged-in player. </summary>
+        /// <returns> A newly-created PlayerInfo entry. </returns>
         [NotNull]
         public PlayerInfo AddPlayer( [NotNull] string name, [NotNull] Rank startingRank, RankChangeType rankChangeType, [NotNull] IPAddress address ) {
             if( name == null ) throw new ArgumentNullException( "name" );
@@ -58,6 +60,8 @@ namespace fCraft {
         }
 
 
+        /// <summary> Adds a new PlayerInfo entry for a player who has never been online, by name. </summary>
+        /// <returns> A newly-created PlayerInfo entry. </returns>
         [NotNull]
         public PlayerInfo AddUnrecognizedPlayer( [NotNull] string name, [NotNull] Rank startingRank, RankChangeType rankChangeType ) {
             if( name == null ) throw new ArgumentNullException( "name" );
@@ -69,16 +73,33 @@ namespace fCraft {
         }
 
 
-        public void Import( [NotNull] PlayerInfo playerInfo ) { }
-        public void Import( [NotNull] IEnumerable<PlayerInfo> playerInfo ) { }
+        /// <summary> Inserts all data from given playerInfo directly into the database. </summary>
+        /// <param name="playerInfo"> Player record to import. </param>
+        public void Import( [NotNull] PlayerInfo playerInfo ) {
+            trie.Add( playerInfo.Name, playerInfo );
+        }
 
 
+        /// <summary> Inserts all data from given PlayerInfo list directly into the database. </summary>
+        /// <param name="playerInfo"> List of player record to import. </param>
+        public void Import( [NotNull] IEnumerable<PlayerInfo> playerInfos ) {
+            foreach( PlayerInfo info in playerInfos ) {
+                trie.Add( info.Name, info );
+            }
+        }
+
+
+        /// <summary> Removes a PlayerInfo entry from the database. </summary>
+        /// <returns> True if the entry is successfully found and removed; otherwise false. </returns>
         public bool Remove( [NotNull] PlayerInfo playerInfo ) {
             if( playerInfo == null ) throw new ArgumentNullException( "playerInfo" );
             return trie.Remove( playerInfo.Name );
         }
 
 
+        /// <summary> Finds player by exact name. </summary>
+        /// <param name="fullName"> Full, case-insensitive name of the player. </param>
+        /// <returns> PlayerInfo if player was found, or null if not found. </returns>
         [CanBeNull]
         public PlayerInfo FindExact( [NotNull] string fullName ) {
             if( fullName == null ) throw new ArgumentNullException( "fullName" );
@@ -88,14 +109,10 @@ namespace fCraft {
         }
 
 
-        public bool FindOneByPartialName( [NotNull] string partialName, [CanBeNull] out PlayerInfo result ) {
-            if( partialName == null ) throw new ArgumentNullException( "partialName" );
-            lock( trie.SyncRoot ) {
-                return trie.GetOneMatch( partialName, out result );
-            }
-        }
-
-
+        /// <summary> Finds players by IP address. </summary>
+        /// <param name="address"> Player's IP address. </param>
+        /// <param name="limit"> Maximum number of results to return. </param>
+        /// <returns> A sequence of zero or more PlayerInfos who have logged in from given IP. </returns>
         [NotNull]
         public IEnumerable<PlayerInfo> FindByIP( [NotNull] IPAddress address, int limit ) {
             if( address == null ) throw new ArgumentNullException( "address" );
@@ -111,6 +128,10 @@ namespace fCraft {
         }
 
 
+        /// <summary> Finds players by partial name (prefix). </summary>
+        /// <param name="partialName"> Full or partial name of the player. </param>
+        /// <param name="limit"> Maximum number of results to return. </param>
+        /// <returns> A sequence of zero or more PlayerInfos whose names start with partialName. </returns>
         [NotNull]
         public IEnumerable<PlayerInfo> FindByPartialName( [NotNull] string partialName, int limit ) {
             if( partialName == null ) throw new ArgumentNullException( "partialName" );
@@ -120,9 +141,27 @@ namespace fCraft {
         }
 
 
+        /// <summary> Searches for player names starting with namePart, returning just one or none of the matches. </summary>
+        /// <param name="partialName"> Partial or full player name. </param>
+        /// <param name="result"> PlayerInfo to output (will be set to null if no single match was found). </param>
+        /// <returns> true if one or zero matches were found, false if multiple matches were found. </returns>
+        public bool FindOneByPartialName( [NotNull] string partialName, [CanBeNull] out PlayerInfo result ) {
+            if( partialName == null ) throw new ArgumentNullException( "partialName" );
+            lock( trie.SyncRoot ) {
+                return trie.GetOneMatch( partialName, out result );
+            }
+        }
+
         [NotNull]
         static readonly Regex RegexNonNameChars = new Regex( @"[^a-zA-Z0-9_\*\?]", RegexOptions.Compiled );
 
+
+        /// <summary> Finds player by name pattern. </summary>
+        /// <param name="pattern"> Pattern to search for.
+        /// Asterisk (*) matches zero or more characters.
+        /// Question mark (?) matches exactly one character. </param>
+        /// <param name="limit"> Maximum number of results to return. </param>
+        /// <returns> A sequence of zero or more PlayerInfos whose names match the pattern. </returns>
         [NotNull]
         public IEnumerable<PlayerInfo> FindByPattern( [NotNull] string pattern, int limit ) {
             if( pattern == null ) throw new ArgumentNullException( "pattern" );
@@ -140,6 +179,7 @@ namespace fCraft {
         }
 
 
+        /// <summary> Changes ranks of all players in one transaction. </summary>
         public void MassRankChange( [NotNull] Player player, [NotNull] Rank from, [NotNull] Rank to, [NotNull] string reason ) {
             if( player == null ) throw new ArgumentNullException( "player" );
             if( from == null ) throw new ArgumentNullException( "from" );
@@ -149,6 +189,7 @@ namespace fCraft {
         }
 
 
+        /// <summary> Swaps records of two players in one transaction. </summary>
         public void SwapInfo( [NotNull] PlayerInfo player1, [NotNull] PlayerInfo player2 ) {
             if( player1 == null ) throw new ArgumentNullException( "player1" );
             if( player2 == null ) throw new ArgumentNullException( "player2" );
@@ -165,6 +206,7 @@ namespace fCraft {
 
         Dictionary<int, Rank> rankMapping;
 
+        /// <summary> Initializes the provider, and allocates PlayerInfo objects for all players. </summary>
         [CanBeNull]
         public IEnumerable<PlayerInfo> Load() {
             //LoadBinary();
@@ -520,6 +562,7 @@ namespace fCraft {
 
         #region Saving
 
+        /// <summary> Saves the whole database. </summary>
         public void Save() {
             const string tempFileName = Paths.PlayerDBFileName + ".bin.temp";
 
