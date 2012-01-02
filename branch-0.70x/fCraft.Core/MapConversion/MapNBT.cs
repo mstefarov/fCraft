196d80a5efc1edb059b3acec0de172193ac3166f
zaneo <zaneo@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-01-02 06:50:27 +0000

    Small commit, preventing possible merging problems.

--- branch-0.70x/fCraft.Core/MapConversion/MapNBT.cs
+++ branch-0.70x/fCraft.Core/MapConversion/MapNBT.cs
@@ -6,28 +6,34 @@ using System.IO.Compression;
 namespace fCraft.MapConversion {
     /// <summary> NBT map conversion implementation, for converting NBT map format into fCraft's default map format. </summary>
     public sealed class MapNBT : IMapConverter {
-
+        /// <summary> Returns name(s) of the server(s) that uses this format. </summary>
         public string ServerName {
             get { return "InDev"; }
         }
 
 
+        /// <summary> Returns the map storage type (file-based or directory-based). </summary>
         public MapStorageType StorageType {
             get { return MapStorageType.SingleFile; }
         }
 
 
+        /// <summary> Returns the format name. </summary>
         public MapFormat Format {
             get { return MapFormat.NBT; }
         }
 
-
+        /// <summary> Whether or not the specified file is used by this format, determined by file extension. </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
         public bool ClaimsName( string fileName ) {
             if( fileName == null ) throw new ArgumentNullException( "fileName" );
             return fileName.EndsWith( ".mclevel", StringComparison.OrdinalIgnoreCase );
         }
 
-
+        /// <summary> Whether or not the specified file is used by this format, determined by Identifier in file. </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
         public bool Claims( string fileName ) {
             if( fileName == null ) throw new ArgumentNullException( "fileName" );
             try {
@@ -41,7 +47,9 @@ namespace fCraft.MapConversion {
             }
         }
 
-
+        /// <summary> Loads the specified file, and creates a map with the specified dimension. </summary>
+        /// <param name="fileName"> File to load. </param>
+        /// <returns> Map instance from the specified file. </returns>
         public Map LoadHeader( string fileName ) {
             if( fileName == null ) throw new ArgumentNullException( "fileName" );
             Map map = Load( fileName );
@@ -49,7 +57,9 @@ namespace fCraft.MapConversion {
             return map;
         }
 
-
+        /// <summary> Loads the specified file, and creates a map with the specified dimensions, and spawn point. </summary>
+        /// <param name="fileName"> File to load. </param>
+        /// <returns> Map instance from the specified file. </returns>
         public Map Load( string fileName ) {
             if( fileName == null ) throw new ArgumentNullException( "fileName" );
             using( FileStream mapStream = File.OpenRead( fileName ) ) {
@@ -83,7 +93,10 @@ namespace fCraft.MapConversion {
             }
         }
 
-
+        /// <summary> Saves the specified map, with the specified file name. </summary>
+        /// <param name="mapToSave"> Map to save. </param>
+        /// <param name="fileName"> File name to save the map under. </param>
+        /// <returns> Whether the operation completed successfully. </returns>
         public bool Save( Map mapToSave, string fileName ) {
             if( mapToSave == null ) throw new ArgumentNullException( "mapToSave" );
             if( fileName == null ) throw new ArgumentNullException( "fileName" );
