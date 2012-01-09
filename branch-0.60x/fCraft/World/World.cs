1ce0f23e56c587c18951db66c65485025455dda4
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-01-09 18:35:41 +0000

    Fixed formatting in "Player ___ got auto-kicked" message (thanks BobKare) (backport from 0.70x) Fixed auto-kick revealing hidden players (thanks BobKare again) (backport from 0.70x) Fixed IRC bot not retrying for desired nickname after reconnecting (backport from 0.70x) Updated copyright notices to 2009-2012.

--- branch-0.60x/fCraft/World/World.cs
+++ branch-0.60x/fCraft/World/World.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2009, 2010, 2011 Matvei Stefarov <me@matvei.org>
+﻿// Copyright 2009-2012 Matvei Stefarov <me@matvei.org>
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -268,8 +268,15 @@ namespace fCraft {
                         if( idlestPlayer != null ) {
                             idlestPlayer.Kick( Player.Console, "Auto-kicked to make room (idle).",
                                                LeaveReason.IdleKick, false, false, false );
-                            Server.Message( "Player {0}&S was auto-kicked to make room for {1}",
+
+                            Server.Players
+                                  .CanSee( player )
+                                  .Message( "&SPlayer {0}&S was auto-kicked to make room for {1}",
                                             idlestPlayer.ClassyName, player.ClassyName );
+                            Server.Players
+                                  .CantSee( player )
+                                  .Message( "{0}&S was kicked for being idle for {1} min",
+                                            player.ClassyName, player.Info.Rank.IdleKickTimer );
                         } else {
                             return null;
                         }
