ef6d710c475830f90513975a71752b8c99859868
Matvei Stefarov <me@matvei.org>
2012-01-25 06:11:36 +0000

    0.614 final (fixed)  Fixed autoupdater freezing when attempting to "Restart Now".  Improved ServerGUI responsiveness when shutting down.

--- ServerGUI/UpdateWindow.cs
+++ ServerGUI/UpdateWindow.cs
@@ -69,7 +69,7 @@ namespace fCraft.ServerGUI {
             string args = Server.GetArgString() +
                           String.Format( "--restart=\"{0}\"", MonoCompat.PrependMono( "ServerGUI.exe" ) );
             MonoCompat.StartDotNetProcess( updaterFullPath, args, true );
-            Application.Exit();
+            Server.Shutdown( new ShutdownParams( ShutdownReason.Restarting, TimeSpan.Zero, true, false ), false );
         }
 
 
