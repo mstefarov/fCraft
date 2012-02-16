f4da5be4587867e1674eb12517b739bd9737a648
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-02-16 18:04:40 +0000

    Removed unneeded assembly reference from fCraft.Core Fixed a formatting error in /Bring and /WBring confirmation messages.

--- branch-0.70x/fCraft.Core/Commands/ModerationCommands.cs
+++ branch-0.70x/fCraft.Core/Commands/ModerationCommands.cs
@@ -883,8 +883,7 @@ namespace fCraft {
                         } else {
                             player.Confirm( cmd,
                                             "Player {0}&S is ranked too low to join {1}&S. Override world permissions?",
-                                            target.Name,
-                                            world );
+                                            target.ClassyName, world.ClassyName );
                         }
                     } else {
                         player.Message( "Neither you nor {0}&S are allowed to join world {1}",
@@ -948,8 +947,7 @@ namespace fCraft {
                     } else {
                         player.Confirm( cmd,
                                         "Player {0}&S is ranked too low to join {1}&S. Override world permissions?",
-                                        target.Name,
-                                        world );
+                                        target.ClassyName, world.ClassyName );
                     }
                 } else {
                     player.Message( "Neither you nor {0}&S are allowed to join world {1}",
