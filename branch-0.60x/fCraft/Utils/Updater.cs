bfbed7b4b9af3c472defad037dd87bb2fe20cf5a
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-03-10 06:37:18 +0000

    Fixed a formatting error in /Bring and /WBring confirmation messages (backport from 0.70x) Fixed mixed-up messages in /waccess and /wbuild when removing players from blacklist/whitelist (thanks ruggedbear and xN8Gx) (backport from 0.70x)

--- branch-0.60x/fCraft/Utils/Updater.cs
+++ branch-0.60x/fCraft/Utils/Updater.cs
@@ -17,7 +17,7 @@ namespace fCraft {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             616,
-            1495,
+            1496,
             new DateTime( 2012, 2, 3, 1, 0, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Bugfix | ReleaseFlags.Dev
