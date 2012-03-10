553ea8a248085edd1304f52d14580772d15cfe0a
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-03-10 00:00:34 +0000

    Rewrote MapConverter to use Mono.Options, and be more flexible.

--- branch-0.70x/MapConverter/Program.cs
+++ branch-0.70x/MapConverter/Program.cs
@@ -1,120 +1,212 @@
 ﻿// Part of fCraft | Copyright 2009-2012 Matvei Stefarov <me@matvei.org> | MIT License
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using fCraft.MapConversion;
 using fCraft.Events;
+using Mono.Options;
 
 namespace fCraft.MapConverter {
     static class Program {
-        const int ExpectedArgs = 4;
         static IMapConverter[] allConverters;
 
+        static string importerName,
+                      exporterName,
+                      inputPath,
+                      outputDirName,
+                      inputFilter;
+
+        static IMapConverter importer;
+        static IMapConverter exporter;
+
+        static bool recursive;
+
+        static OptionSet opts;
+
+
         static int Main( string[] args ) {
-            // init fCraft
             Logger.Logged += OnLogged;
-            Server.InitLibrary( new string[0] );
 
             allConverters = MapUtility.GetConverters();
 
-            // check number of arguments
-            if( args.Length != ExpectedArgs ) {
-                PrintUsage();
-                return (int)ReturnCode.WrongArgCount;
+            ParseOptions( args );
+
+            // parse importer name
+            if( importerName != null ) {
+                MapFormat importFormat;
+                if( !EnumUtil.TryParse( importerName, out importFormat, true ) ||
+                    ( importer = MapUtility.GetConverter( importFormat ) ) == null ) {
+                    Console.Error.WriteLine( "Unrecognized importer \"{0}\"", importerName );
+                    PrintUsage();
+                    return (int)ReturnCode.UnrecognizedImporter;
+                }
             }
 
-            string fromFormatName = args[0];
-            string toFormatName = args[1];
-            string inputPath = args[2];
-            string outputPath = args[3];
-
-            // parse from-format
-            MapFormat fromFormat;
-            if( !EnumUtil.TryParse( fromFormatName, out fromFormat, true ) ) {
-                Console.Error.WriteLine( "Unrecognized format \"{0}\"", fromFormatName );
+            // parse exporter format
+            MapFormat exportFormat;
+            if( !EnumUtil.TryParse( exporterName, out exportFormat, true ) ||
+                ( exporter = MapUtility.GetConverter( exportFormat ) ) == null ) {
+                Console.Error.WriteLine( "Unrecognized exporter \"{0}\"", exporterName );
                 PrintUsage();
-                return (int)ReturnCode.UnrecognizedFromFormat;
+                return (int)ReturnCode.UnrecognizedExporter;
             }
-
-            // parse to-format
-            MapFormat toFormat;
-            if( !EnumUtil.TryParse( toFormatName, out toFormat, true ) ) {
-                Console.Error.WriteLine( "Unrecognized format \"{0}\"", toFormatName );
-                PrintUsage();
-                return (int)ReturnCode.UnrecognizedToFormat;
-            }
-
-            // get converters
-            IMapConverter fromConverter = MapUtility.GetConverter( fromFormat );
-            IMapConverter toConverter = MapUtility.GetConverter( toFormat );
-            if( !toConverter.SupportsExport ) {
-                var supportedConverters = allConverters.Where( c => c.SupportsExport );
-                Console.Error.WriteLine( "fCraft does not support exporting to {0} format. Supported formats: {1}",
-                                         toFormat,
-                                         supportedConverters.JoinToString( c => c.Format.ToString() ) );
+            if( !exporter.SupportsExport ) {
+                Console.Error.WriteLine( "fCraft does not support exporting to {0} format.", exporter );
                 return (int)ReturnCode.UnsupportedSaveFormat;
             }
 
-            // try to open the input directory
-            DirectoryInfo inputDir;
+            // check if input path exists, and if it's a file or directory
+            bool directoryMode;
+            string outputDirectory;
             try {
-                inputDir = new DirectoryInfo( inputPath );
-            } catch( Exception ex ) {
-                Console.Error.WriteLine( "{0}: {1}", ex.GetType().Name, ex.Message );
-                return (int)ReturnCode.ErrorOpeningDirForLoading;
-            }
+                if( File.Exists( inputPath ) ) {
+                    directoryMode = false;
+                    outputDirectory = Path.GetDirectoryName( Path.GetFullPath( inputPath ) );
 
-            // verify that load directory exists
-            if( !inputDir.Exists ) {
-                Console.Error.WriteLine( "Can't find {0}", inputPath );
-                PrintUsage();
-                return (int)ReturnCode.InputDirNotFound;
-            }
+                } else if( Directory.Exists( inputPath ) ) {
+                    directoryMode = true;
+                    outputDirectory = Path.GetFullPath( inputPath );
 
-            // try to open the output directory
-            LogRecorder logger = new LogRecorder();
-            if( !Paths.TestDirectory( "output", outputPath, true ) ) {
-                if( logger.HasMessages ) Console.Error.WriteLine( logger.MessageString );
-                return (int)ReturnCode.ErrorOpeningDirForSaving;
-            }
-
-            if( fromConverter.StorageType == MapStorageType.Directory ) {
-                // go through all directories
-                foreach( var subdir in inputDir.EnumerateDirectories() ) {
-                    try {
-                        Console.Write( "Loading {0}... ", subdir.Name );
-                        string targetName = Path.Combine( outputPath, subdir.Name + '.' + toConverter.FileExtension );
-                        Map map = fromConverter.Load( subdir.FullName );
-                        Console.Write( "Saving {0}... ", Path.GetFileName( targetName ) );
-                        toConverter.Save( map, targetName );
-                        Console.WriteLine( "ok" );
-                    } catch( Exception ex ) {
-                        Console.Error.WriteLine( "ERROR: {0}: {1}", ex.GetType().Name, ex.Message );
-                    }
+                } else {
+                    Console.Error.WriteLine( "MapConverter: Cannot locate \"{0}\"", inputPath );
+                    return (int)ReturnCode.InputDirNotFound;
                 }
+
+                if( outputDirName != null ) {
+                    outputDirectory = Path.GetFullPath( outputDirName );
+                }
+            } catch( Exception ex ) {
+                Console.Error.WriteLine( "MapConverter: {0}: {1}",
+                                         ex.GetType().Name,
+                                         ex.Message );
+                return (int)ReturnCode.PathError;
+            }
+
+            // check recursive flag
+            if( recursive && !directoryMode ) {
+                Console.Error.WriteLine( "MapConverter: Recursive flag is given, but input is not a directory." );
+            }
+
+            // check recursive flag
+            if( inputFilter != null && !directoryMode ) {
+                Console.Error.WriteLine( "MapConverter: Filter param is given, but input is not a directory." );
+            }
+
+
+            if( importer != null && importer.StorageType == MapStorageType.Directory ) {
+                ConvertOneMap( outputDirectory, new DirectoryInfo( inputPath ) );
+            } else if( !directoryMode ) {
+                ConvertOneMap( outputDirectory, new FileInfo( inputPath ) );
             } else {
-                // go through all files
-                foreach( var fileInfo in inputDir.EnumerateFiles() ) {
-                    try {
-                        if( !fromConverter.ClaimsName( fileInfo.FullName ) ) {
-                            Console.WriteLine( "Skipping {0}", fileInfo.Name );
-                            continue;
-                        }
-                        Console.Write( "Loading {0}... ", fileInfo.Name );
-                        string targetName = Path.Combine( outputPath, Path.GetFileNameWithoutExtension( fileInfo.Name ) + '.' + toConverter.FileExtension );
-                        Map map = fromConverter.Load( fileInfo.FullName );
-                        Console.Write( "Saving {0}... ", Path.GetFileName( targetName ) );
-                        map.Save( targetName );
-                        Console.WriteLine( "ok" );
-                    } catch( Exception ex ) {
-                        Console.WriteLine( "ERROR" );
-                        Console.Error.WriteLine( "{0}: {1}", ex.GetType().Name, ex.Message );
-                    }
+                SearchOption recursiveOption = ( recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly );
+                DirectoryInfo inputDirInfo = new DirectoryInfo( inputPath );
+                if( inputFilter == null ) inputFilter = "*";
+                foreach( FileSystemInfo dirInfo in inputDirInfo.EnumerateFileSystemInfos( inputFilter, recursiveOption )
+                    ) {
+                    ConvertOneMap( outputDirectory, dirInfo );
                 }
             }
 
             return (int)ReturnCode.Success;
         }
+
+
+        static void ConvertOneMap( string outputDir, FileSystemInfo fileSystemInfo ) {
+            try {
+                if( !importer.ClaimsName( fileSystemInfo.FullName ) ) {
+                    Console.WriteLine( "Skipping {0}", fileSystemInfo.Name );
+                    return;
+                }
+
+                Console.Write( "Loading {0}... ", fileSystemInfo.Name );
+                Map map;
+                if( importer != null ) {
+                    map = importer.Load( fileSystemInfo.FullName );
+                } else {
+                    map = MapUtility.Load( fileSystemInfo.FullName );
+                }
+
+                string targetFileName;
+                if( ( fileSystemInfo.Attributes & FileAttributes.Directory ) == FileAttributes.Directory ) {
+                    targetFileName = fileSystemInfo.Name + '.' + exporter.FileExtension;
+                } else {
+                    targetFileName = Path.GetFileNameWithoutExtension( fileSystemInfo.Name ) + '.' + exporter.FileExtension;
+                }
+                Console.Write( "Saving {0}... ", Path.GetFileName( targetFileName ) );
+                exporter.Save( map, Path.Combine( outputDir, targetFileName ) );
+                Console.WriteLine( "ok" );
+
+            } catch( Exception ex ) {
+                Console.WriteLine( "ERROR" );
+                Console.Error.WriteLine( "{0}: {1}", ex.GetType().Name, ex.Message );
+            }
+        }
+
+
+        static void ParseOptions( string[] args ) {
+            string importerList = allConverters.JoinToString( c => c.Format.ToString() );
+            string exporterList = allConverters.Where( c => c.SupportsExport ).JoinToString( c => c.Format.ToString() );
+
+            bool printHelp = false;
+
+            opts = new OptionSet()
+                .Add( "?|help|h",
+                      "Prints out the options.",
+                      o => printHelp = ( o != null ) )
+
+                .Add( "i=|importer=",
+                      "Optional: Converter used for importing/loading maps. " +
+                      "Available importers: " + importerList,
+                      o => importerName = o )
+
+                .Add( "e=|exporter=",
+                      "REQUIRED: Converter used for exporting/saving maps. " +
+                      "Available exporters: " + exporterList,
+                      o => exporterName = o )
+
+                .Add( "o=|output=",
+                      "Optional: Path to save converted map files to. " +
+                      "If not specified, converted maps will be saved to the original maps' directory.",
+                      o => outputDirName = o )
+
+                .Add( "r|recursive",
+                      "Optional: Look through all subdirectories, and convert map files there too.",
+                      o => recursive = ( o != null ) )
+
+                .Add( "f=|filter=",
+                      "Optional: Pattern to filter input filenames, e.g. \"*.dat\" or \"builder*\"",
+                      o => inputFilter = o );
+
+            List<string> pathList = new List<string>();
+            try {
+                pathList = opts.Parse( args );
+            } catch( OptionException ex ) {
+                Console.Error.Write( "MapConverter: " );
+                Console.Error.WriteLine( ex.Message );
+                PrintUsage();
+                Environment.Exit( (int)ReturnCode.ArgumentParsingError );
+            }
+
+            if( printHelp ) {
+                PrintUsage();
+                Environment.Exit( (int)ReturnCode.Success );
+            }
+
+            if( pathList.Count != 1 ) {
+                Console.Error.WriteLine( "MapConverter: At least one file or directory name required." );
+                PrintUsage();
+                Environment.Exit( (int)ReturnCode.ArgumentParsingError );
+            }
+            inputPath = pathList[0];
+
+            if( exporterName == null ) {
+                Console.Error.WriteLine( "MapConverter: Export format required." );
+                PrintUsage();
+                Environment.Exit( (int)ReturnCode.ArgumentParsingError );
+            }
+        }
+
 
         static void OnLogged( object sender, LogEventArgs e ) {
             switch( e.MessageType ) {
@@ -131,21 +223,17 @@ namespace fCraft.MapConverter {
 
 
         static void PrintUsage() {
-            Console.WriteLine( "Usage: MapConverter InputFormat OutputFormat \"LoadPath\" \"SavePath\"" );
-            Console.WriteLine( "Supported input formats: {0}",
-                               allConverters.JoinToString( c => c.Format.ToString() ) );
-            Console.WriteLine( "Supported output formats: {0}",
-                               allConverters.Where( c => c.SupportsExport ).JoinToString( c => c.Format.ToString() ) );
+            opts.WriteOptionDescriptions( Console.Out );
         }
     }
 
     enum ReturnCode {
         Success = 0,
-        WrongArgCount = 1,
-        UnrecognizedFromFormat = 2,
-        UnrecognizedToFormat = 3,
+        ArgumentParsingError = 1,
+        UnrecognizedImporter = 2,
+        UnrecognizedExporter = 3,
         InputDirNotFound = 4,
-        ErrorOpeningDirForLoading = 5,
+        PathError = 5,
         ErrorOpeningDirForSaving = 6,
         UnsupportedSaveFormat = 7
     }
