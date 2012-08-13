285018d457c616bd743be710465a28a02c4b59b6
Matvei Stefarov <me@matvei.org>
2012-08-13 19:40:49 +0000

    Fixed a regression crash in /Info

--- fCraft/World/Block.cs
+++ fCraft/World/Block.cs
@@ -3,7 +3,7 @@
 namespace fCraft {
     /// <summary> Enumeration of all standard Minecraft Classic block types. </summary>
     public enum Block : byte {
-        None = 255, // for error checking
+        None = 255,
 
         Air = 0,
         Stone = 1,
