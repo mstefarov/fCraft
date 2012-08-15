43a96a736b7edb8499099eb232ccddf459a5321d
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-08-15 03:50:36 +0000

    Got rid of an unnecessary layer of abstraction, and improved feedback messages in /UndoPlayer.

--- branch-0.60x/fCraft/Drawing/DrawOps/BlockDBDrawOperation.cs
+++ branch-0.60x/fCraft/Drawing/DrawOps/BlockDBDrawOperation.cs
@@ -2,29 +2,78 @@
 using System;
 
 namespace fCraft.Drawing {
-    public abstract class BlockDBDrawOperation : DrawOpWithBrush {
-        public override string Description {
-            get { return Name; }
+    public class BlockDBDrawOperation : DrawOpWithBrush {
+        public override string Name {
+            get { return commandName; }
         }
 
-        public override abstract string Name { get; }
+        public override int ExpectedMarks {
+            get { return expectedMarks; }
+        }
+        int expectedMarks;
+
         protected BlockDBEntry[] entriesToUndo;
+        int entryIndex;
+        Block block;
+        string commandName;
 
 
-        protected BlockDBDrawOperation( Player player )
-            : base( player ) {
+        public override string Description {
+            get {
+                if( String.IsNullOrEmpty( UndoParamDescription ) ) {
+                    return Name;
+                } else {
+                    return String.Format( "{0}({1})", Name, UndoParamDescription );
+                }
+            }
         }
 
+        public string UndoParamDescription { get; set; }
+
+
+        public BlockDBDrawOperation( Player player, string commandName, int expectedMarks )
+            : base( player ) {
+            if( commandName == null ) throw new ArgumentNullException( "commandName" );
+            this.commandName = commandName;
+            this.expectedMarks = expectedMarks;
+        }
+
+
+        public bool Prepare( Vector3I[] marks, BlockDBEntry[] entriesToUndo ) {
+            if( entriesToUndo == null ) throw new ArgumentNullException( "entriesToUndo" );
+            this.entriesToUndo = entriesToUndo;
+            return Prepare( marks );
+        }
 
         public override bool Prepare( Vector3I[] marks ) {
+            if( entriesToUndo == null ) {
+                throw new InvalidOperationException( "Call the other overload to set entriesToUndo" );
+            }
             Brush = this;
             if( !base.Prepare( marks ) ) return false;
             BlocksTotalEstimate = entriesToUndo.Length;
+            if( marks.Length != 2 ) {
+                Bounds = FindBounds();
+            }
             return true;
         }
 
-        int entryIndex;
-        Block block;
+
+        BoundingBox FindBounds() {
+            if( entriesToUndo.Length == 0 ) return BoundingBox.Empty;
+            Vector3I min = new Vector3I( int.MaxValue, int.MaxValue, int.MaxValue );
+            Vector3I max = new Vector3I( int.MinValue, int.MinValue, int.MinValue );
+            for( int i = 0; i < entriesToUndo.Length; i++ ) {
+                if( entriesToUndo[i].X < min.X ) min.X = entriesToUndo[i].X;
+                if( entriesToUndo[i].Y < min.Y ) min.Y = entriesToUndo[i].Y;
+                if( entriesToUndo[i].Z < min.Z ) min.Z = entriesToUndo[i].Z;
+                if( entriesToUndo[i].X > max.X ) max.X = entriesToUndo[i].X;
+                if( entriesToUndo[i].Y > max.Y ) max.Y = entriesToUndo[i].Y;
+                if( entriesToUndo[i].Z > max.Z ) max.Z = entriesToUndo[i].Z;
+            }
+            return new BoundingBox( min, max );
+        }
+
 
         public override int DrawBatch( int maxBlocksToDraw ) {
             int blocksDone = 0;
