a3269323f26889d433ea046e655b9d49d87a873a
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-08-24 11:05:05 +0000

    Ported MapRenderer from 0.70x (untested)

---
+++ branch-0.60x/MapRenderer/ReturnCode.cs
@@ -0,0 +1,12 @@
+// Part of fCraft | Copyright (c) 2009-2012 Matvei Stefarov <me@matvei.org> | BSD-3 | See LICENSE.txt
+
+namespace fCraft.MapRenderer {
+    enum ReturnCode {
+        Success = 0,
+        ArgumentError = 1,
+        UnrecognizedImporter = 2,
+        InputDirNotFound = 3,
+        PathError = 4,
+        UnsupportedSaveFormat = 5
+    }
+}
