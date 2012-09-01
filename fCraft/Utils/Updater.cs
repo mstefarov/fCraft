8708c1879b7e7396d8ef3ee65d9b03ac6e15b792
Matvei Stefarov <me@matvei.org>
2012-09-01 22:17:33 +0000

    Added more logging to the shutdown process. Server is now more patient in waiting for players to disconnect.

--- fCraft/Utils/Updater.cs
+++ fCraft/Utils/Updater.cs
@@ -17,8 +17,8 @@ namespace fCraft {
 
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             630,
-            1706,
-            new DateTime( 2012, 8, 30, 20, 15, 0, DateTimeKind.Utc ),
+            1710,
+            new DateTime( 2012, 9, 1, 22, 0, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Dev | ReleaseFlags.PlayerDBFormatChange | ReleaseFlags.ConfigFormatChange | ReleaseFlags.Bugfix | ReleaseFlags.Feature | ReleaseFlags.Optimized
 #if DEBUG
