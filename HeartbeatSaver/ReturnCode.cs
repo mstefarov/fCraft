a4804233bbf47a58d5d048f2754eeef5911a1e94
Matvei Stefarov <me@matvei.org>
2012-09-24 02:59:50 +0000

    Ported changes from /branch-0.60x/

---
+++ HeartbeatSaver/ReturnCode.cs
@@ -0,0 +1,8 @@
+// Copyright 2009-2012 Matvei Stefarov <me@matvei.org>
+
+namespace fCraft.HeartbeatSaver {
+    enum ReturnCode {
+        UsageError = 1,
+        HeartbeatDataReadingError = 2
+    }
+}
