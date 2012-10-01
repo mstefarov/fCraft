8a9c16ad743014176a19e51d765f3364571465ad
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-10-01 01:56:13 +0000

    Fixed an occasional InvalidOperationException when auto-updating in ServerGUI

--- branch-0.60x/fCraft/System/Config.cs
+++ branch-0.60x/fCraft/System/Config.cs
@@ -772,8 +772,10 @@ namespace fCraft {
         /// Note the parsing is done in a case-insensitive way. </summary>
         /// <typeparam name="TEnum"> Enum to use for parsing.
         /// An ArgumentException will be thrown if this is not an enum. </typeparam>
+        /// <exception cref="ArgumentException"> TEnum is not an System.Enum.
+        /// -or- key value is either an empty string ("") or only contains white space.
+        /// -or- value is a name, but not one of the named constants defined for the enumeration. </exception>
         public static TEnum GetEnum<TEnum>( this ConfigKey key ) where TEnum : struct {
-            if( !typeof( TEnum ).IsEnum ) throw new ArgumentException( "Enum type required" );
             return (TEnum)Enum.Parse( typeof( TEnum ), GetString( key ), true );
         }
 
--- branch-0.60x/fCraft/Utils/Updater.cs
+++ branch-0.60x/fCraft/Utils/Updater.cs
@@ -17,7 +17,7 @@ namespace fCraft {
         /// <summary> The current release information of this version/revision. </summary>
         public static readonly ReleaseInfo CurrentRelease = new ReleaseInfo(
             634,
-            1791,
+            1794,
             new DateTime( 2012, 9, 26, 20, 30, 0, DateTimeKind.Utc ),
             "", "",
             ReleaseFlags.Dev | ReleaseFlags.ConfigFormatChange | ReleaseFlags.Bugfix
