d867cdf2298b6a21128161547e57efb814d06f62
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-10-04 02:05:09 +0000

    Removed branch-0.64x for now, needs to be rebranched.

--- branch-0.64x/HeartbeatSaver/ReturnCode.cs
+++
@@ -1,8 +0,0 @@
-// Copyright 2009-2012 Matvei Stefarov <me@matvei.org>
-
-namespace fCraft.HeartbeatSaver {
-    enum ReturnCode {
-        UsageError = 1,
-        HeartbeatDataReadingError = 2
-    }
-}
