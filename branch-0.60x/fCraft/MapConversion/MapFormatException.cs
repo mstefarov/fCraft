f61741174c0eaf919038299aee86faf22e3c9150
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2012-10-05 18:53:33 +0000

    Removed unnecessary reference to System.Data assembly from fCraft.dll Made MapFormatException and NoMapConverterFoundException serializable.

--- branch-0.60x/fCraft/MapConversion/MapFormatException.cs
+++ branch-0.60x/fCraft/MapConversion/MapFormatException.cs
@@ -1,22 +1,45 @@
 ﻿// Part of fCraft | Copyright (c) 2009-2012 Matvei Stefarov <me@matvei.org> | BSD-3 | See LICENSE.txt
 using System;
+using System.Runtime.Serialization;
 using JetBrains.Annotations;
 
 namespace fCraft.MapConversion {
     /// <summary> Exception that is thrown when problems arise during map saving or loading.
     /// May be caused by map file's incorrect format or structure. </summary>
-    public class MapFormatException : Exception {
+    [Serializable]
+    public sealed class MapFormatException : Exception {
         internal MapFormatException() {}
 
-        internal MapFormatException( [NotNull] string message ) : base( message ) {}
+
+        internal MapFormatException( [NotNull] string message )
+            : base( message ) {}
+
+
+        internal MapFormatException( [NotNull] string message, Exception innerException )
+            : base( message, innerException ) {}
+
+
+        MapFormatException( SerializationInfo info, StreamingContext context )
+            : base( info, context ) {}
     }
 
 
     /// <summary> Exception that is thrown when no importer or exporter could
     /// be found for the given map format, or a map format could not be identified at all. </summary>
-    public sealed class NoMapConverterFoundException : MapFormatException {
+    [Serializable]
+    public sealed class NoMapConverterFoundException : Exception {
         internal NoMapConverterFoundException() {}
 
-        internal NoMapConverterFoundException( [NotNull] string message ) : base( message ) {}
+
+        internal NoMapConverterFoundException( [NotNull] string message )
+            : base( message ) {}
+
+
+        internal NoMapConverterFoundException( [NotNull] string message, Exception innerException )
+            : base( message, innerException ) {}
+
+
+        NoMapConverterFoundException( SerializationInfo info, StreamingContext context )
+            : base( info, context ) {}
     }
 }
