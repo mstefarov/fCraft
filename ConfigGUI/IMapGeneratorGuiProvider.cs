fa833a9d7dd41a289f874c3c1ff659b0b35bf741
Matvei Stefarov <me@matvei.org>
2013-04-08 07:56:59 +0000

    Added IMapGenerator and IMapGeneratorGuiProvider interfaces

---
+++ ConfigGUI/IMapGeneratorGuiProvider.cs
@@ -0,0 +1,13 @@
+﻿// Part of fCraft | Copyright (c) 2009-2012 Matvei Stefarov <me@matvei.org> | BSD-3 | See LICENSE.txt
+using System;
+using System.Windows.Forms;
+
+namespace fCraft.ConfigGUI {
+    interface IMapGeneratorGuiProvider {
+        string Name { get; }
+        string GeneratorName { get; }
+        bool IsCompatible( Version generatorVersion );
+
+        Panel CreateGUI( WorldListEntry world );
+    }
+}
---
+++ fCraft/World/IMapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace fCraft {
+    interface IMapGenerator {
+        string Name { get; }
+        Version Version { get; }
+
+        // raised as the map generation progresses (optional)
+        event ProgressChangedEventHandler ProgressChanged;
+
+        // whether this generator provides a GUI
+        bool ProvidesGui { get; }
+
+        // gets an object representing current generation parameters
+        object GetParameters();
+
+        // sets an object representing generation parameters
+        void SetParameters( object parameters );
+
+        // starts generation!
+        void Generate( int width, int height, int length );
+    }
+}
