aa3452a5474841c0cd900cc6f6c3962728bf583c
fragmer <fragmer@361580da-8db9-4bc3-b993-010fc5d4b379>
2013-04-09 21:53:18 +0000

    Moved all map generation related files to a new /fCraft/MapGeneration/ directory

---
+++ branch-0.60x/fCraft/MapGeneration/IMapGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fCraft {
+    /// <summary> Represents a type of map generator.
+    /// Provides general information about this generator, and ways to create IMapGeneratorParameters objects. </summary>
+    public interface IMapGenerator {
+        /// <summary> Name of the map generator. Uses same rules as command and world names. </summary>
+        string Name { get; }
+
+        /// <summary> Current version of the map generator. </summary>
+        Version Version { get; }
+
+        IMapGeneratorParameters GetDefaultParameters();
+        IMapGeneratorParameters CreateParameters( string args );
+        IMapGeneratorParameters CreateParameters( CommandReader args );
+    }
+}
---
+++ branch-0.60x/fCraft/MapGeneration/IMapGeneratorParameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fCraft {
+    /// <summary> Represets a set of map generator parameters.
+    /// Provides a way to serialize these parameters to string, and a way to create single-use IMapGeneratorState objects. 
+    /// Must be mutable, should implement parameter range validation in property setters, and implement ICloneable,
+    /// and pass a COPY to IMapGeneratorState. </summary>
+    public interface IMapGeneratorParameters : ICloneable {
+        /// <summary> Associated IMapGenerator object that created this parameter set. </summary>
+        IMapGenerator Generator { get; }
+
+        /// <summary> Short summary of current parameter set, or name of this template. </summary>
+        string SummaryString { get; }
+
+        /// <summary> Saves current generation parameters to a string,
+        /// in a format that's expected to be readable by IMapGenerator.CreateParameters(string) </summary>
+        string Save();
+
+        /// <summary> Creates IMapGeneratorState to create a map with the current parameters and specified dimensions. 
+        /// Does NOT start the generation process yet -- that should be done in IMapGeneratorState.Generate() </summary>
+        IMapGeneratorState CreateGenerator( int width, int height, int length );
+    }
+}
