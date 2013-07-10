6ad48d7db174ddf9d6e4f93608df01a5a84d83da
Matvei Stefarov <me@matvei.org>
2013-07-10 15:13:03 +0000

    Minor optimizations in /Restore and /Ellipsoid commands, to reduce temporary allocations.

--- fCraft/Drawing/DrawOps/EllipsoidDrawOperation.cs
+++ fCraft/Drawing/DrawOps/EllipsoidDrawOperation.cs
@@ -5,75 +5,62 @@ using System.Collections.Generic;
 namespace fCraft.Drawing {
     /// <summary> Draw operation that creates a filled ellipsoid. </summary>
     public class EllipsoidDrawOperation : DrawOperation {
-        Vector3F radius, center;
+        Vector3F invRadius,
+                 center;
 
-        public override string Name {
-            get { return "Ellipsoid"; }
-        }
+        public override string Name { get { return "Ellipsoid"; } }
 
-        public override int ExpectedMarks {
-            get { return 2; }
-        }
+        public override int ExpectedMarks { get { return 2; } }
 
         public EllipsoidDrawOperation( Player player )
-            : base( player ) {
-        }
+            : base( player ) {}
 
 
         public override bool Prepare( Vector3I[] marks ) {
             if( !base.Prepare( marks ) ) return false;
 
-            double rx = Bounds.Width / 2d;
-            double ry = Bounds.Length / 2d;
-            double rz = Bounds.Height / 2d;
+            double rx = Bounds.Width/2d;
+            double ry = Bounds.Length/2d;
+            double rz = Bounds.Height/2d;
 
-            radius.X = (float)(1 / (rx * rx));
-            radius.Y = (float)(1 / (ry * ry));
-            radius.Z = (float)(1 / (rz * rz));
+            invRadius.X = (float)(1/(rx*rx));
+            invRadius.Y = (float)(1/(ry*ry));
+            invRadius.Z = (float)(1/(rz*rz));
 
             // find center points
-            center.X = (float)((Bounds.XMin + Bounds.XMax) / 2d);
-            center.Y = (float)((Bounds.YMin + Bounds.YMax) / 2d);
-            center.Z = (float)((Bounds.ZMin + Bounds.ZMax) / 2d);
+            center.X = (Bounds.XMin + Bounds.XMax)/2f;
+            center.Y = (Bounds.YMin + Bounds.YMax)/2f;
+            center.Z = (Bounds.ZMin + Bounds.ZMax)/2f;
 
-            BlocksTotalEstimate = (int)Math.Ceiling( 4 / 3d * Math.PI * rx * ry * rz );
+            BlocksTotalEstimate = (int)Math.Ceiling( 4/3d*Math.PI*rx*ry*rz );
 
-            coordEnumerator = BlockEnumerator().GetEnumerator();
+            Coords = Bounds.MinVertex;
             return true;
         }
 
 
-        IEnumerator<Vector3I> coordEnumerator;
         public override int DrawBatch( int maxBlocksToDraw ) {
             int blocksDone = 0;
-            while( coordEnumerator.MoveNext() ) {
-                Coords = coordEnumerator.Current;
-                if( DrawOneBlock() ) {
-                    blocksDone++;
-                    if( blocksDone >= maxBlocksToDraw ) return blocksDone;
+            for( ; Coords.X <= Bounds.XMax; Coords.X++ ) {
+                for( ; Coords.Y <= Bounds.YMax; Coords.Y++ ) {
+                    for( ; Coords.Z <= Bounds.ZMax; Coords.Z++ ) {
+                        double dx = (Coords.X - center.X);
+                        double dy = (Coords.Y - center.Y);
+                        double dz = (Coords.Z - center.Z);
+
+                        // test if it's inside ellipse
+                        if( (dx*dx)*invRadius.X + (dy*dy)*invRadius.Y + (dz*dz)*invRadius.Z <= 1 ) {
+                            if( DrawOneBlock() ) {
+                                blocksDone++;
+                                if( blocksDone >= maxBlocksToDraw ) return blocksDone;
+                            }
+                        }
+                    }
+                    if( TimeToEndBatch ) return blocksDone;
                 }
-                if( TimeToEndBatch ) return blocksDone;
             }
             IsDone = true;
             return blocksDone;
-        }
-
-
-        IEnumerable<Vector3I> BlockEnumerator() {
-            for( int x = Bounds.XMin; x <= Bounds.XMax; x++ ) {
-                for( int y = Bounds.YMin; y <= Bounds.YMax; y++ ) {
-                    for( int z = Bounds.ZMin; z <= Bounds.ZMax; z++ ) {
-                        double dx = (x - center.X);
-                        double dy = (y - center.Y);
-                        double dz = (z - center.Z);
-
-                        // test if it's inside ellipse
-                        if( (dx * dx) * radius.X + (dy * dy) * radius.Y + (dz * dz) * radius.Z <= 1 ) {
-                            yield return new Vector3I( x, y, z );
-                        }
-                    }
-                }
-            }
         }
     }
 }
