21e823c4426341dc60168f36bb310f1d31fa2ced
Matvei Stefarov <me@matvei.org>
2014-08-01 16:08:07 +0000

    Added support for exporting maps in D3Folder format.

--- fCraft/MapConversion/MapD3Folder.cs
+++ fCraft/MapConversion/MapD3Folder.cs
@@ -5,13 +5,46 @@ using System.IO.Compression;
 using JetBrains.Annotations;
 
 namespace fCraft.MapConversion {
-    public class MapD3Folder : IMapImporter {
+    public class MapD3Folder : IMapImporter, IMapExporter {
         const string DataFileName = "Data-Layer.gz";
         const string ConfigFileName = "Config.txt";
 
         public string ServerName { get; private set; }
         public MapStorageType StorageType { get; private set; }
         public MapFormat Format { get; private set; }
+
+
+        public void Save(Map mapToSave, string path) {
+            if( mapToSave == null ) throw new ArgumentNullException("mapToSave");
+            if( path == null ) throw new ArgumentNullException("path");
+
+            if( !Directory.Exists(path) ) {
+                Directory.CreateDirectory(path);
+            }
+            using( StreamWriter sw = File.CreateText(Path.Combine(path, ConfigFileName)) ) {
+                sw.WriteLine("; Converted by {0} on {1}", Updater.UserAgent, DateTime.Now.ToCompactString());
+                sw.WriteLine("Size_X = {0}", mapToSave.Width);
+                sw.WriteLine("Size_Y = {0}", mapToSave.Length);
+                sw.WriteLine("Size_Z = {0}", mapToSave.Height);
+                sw.WriteLine("Spawn_X = {0}", FromPositionCoord(mapToSave.Spawn.X));
+                sw.WriteLine("Spawn_Y = {0}", FromPositionCoord(mapToSave.Spawn.Y));
+                sw.WriteLine("Spawn_Z = {0}", FromPositionCoord(mapToSave.Spawn.Z));
+                sw.WriteLine("Spawn_Rot = {0}", FromPositionAngle(mapToSave.Spawn.R));
+                sw.WriteLine("Spawn_Look = {0}", FromPositionAngle(mapToSave.Spawn.L));
+            }
+            using( FileStream fs = File.Create(Path.Combine(path, DataFileName)) ) {
+                using( GZipStream gs = new GZipStream(fs, CompressionMode.Compress) ) {
+                    // Write the map data
+                    for( int i = 0; i < mapToSave.Volume; i++ ) {
+                        gs.WriteByte(mapToSave.Blocks[i]);
+                        gs.WriteByte(0); // permission byte
+                        gs.WriteByte(0xFF); // ownership short
+                        gs.WriteByte(0xFF);
+                    }
+                }
+            }
+        }
+
 
         public string FileExtension {
             get { throw new NotSupportedException(); }
@@ -75,13 +108,21 @@ namespace fCraft.MapConversion {
 
 
         static byte ToPositionAngle(string str) {
-            int binDegrees = (int)Math.Round(Double.Parse(str)/360*256);
+            int binDegrees = (int)Math.Round(Double.Parse(str)/360d*256d);
             return (byte)(binDegrees < 0 ? (256 + binDegrees) : binDegrees);
+        }
+
+        static double FromPositionAngle(int angle) {
+            double degrees = angle/256d*360d;
+            return (degrees > 180 ? degrees - 360 : degrees);
         }
 
 
         static short ToPositionCoord(string str) {
             return (short)(Double.Parse(str)*32);
+        }
+        static double FromPositionCoord(short coord) {
+            return coord/32d;
         }
 
 
