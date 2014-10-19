62b3b68341bf3ef8d42387236224e93b4a35b45b
Matvei Stefarov <me@matvei.org>
2014-10-19 17:17:37 +0000

    Added Upgrader project stub.

---
+++ Upgrader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fCraft.Upgrader {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Title = "fCraft Upgrader (0.6xx to 0.900)";
+            // 1) check if installation has already been upgraded
+            // 2) check which files are present
+            // 3) check for write access
+            // 4) ask for confirmation
+            // 5) map conversion
+            // 6) config conversion
+            // 7) file relocation
+        }
+    }
+}
